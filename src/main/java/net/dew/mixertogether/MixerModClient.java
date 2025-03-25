c412db988552962fb399ee26e5ca01ad408528f5
dEWGAMIING01 <dewgamingshool@gmai.com>
2025-03-25 23:02:02 +0700

    test to commit

---
+++ src/main/java/net/dew/mixertogether/MixerModClient.java
@@ -0,0 +1,10 @@
+package net.dew.mixertogether;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MixerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dew/mixertogether/MixerTogetherDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dew.mixertogether;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MixerTogetherDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
